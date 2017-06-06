9c19cfff12148c5ea4737e64a9517a346b9e1c3e
Jakob Jul Elben <elbenjakobjul@gmail.com>
2017-06-06 16:00:22 +0200

    add a helper function to retrieve api url's

---
+++ R/utils.R
@@ -0,0 +1,13 @@
+url <- function(type) {
+  arg_match(type, c("subjects", "tables", "tableinfo", "data"))
+
+  if(type == "subject") {
+    "http://api.statbank.dk/v1/subjects"
+  } else if (type == "tables") {
+    "http://api.statbank.dk/v1/tables"
+  } else if (type == "tableinfo") {
+    "http://api.statbank.dk/v1/tableinfo"
+  } else {
+    "http://api.statbank.dk/v1/data"
+  }
+}
