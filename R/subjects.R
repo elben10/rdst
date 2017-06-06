48af0bafd8afb9ac24ba62bf6eabb943fbe31166
Jakob Jul Elben <elbenjakobjul@gmail.com>
2017-06-06 17:30:25 +0200

    update query for consistency

--- R/subjects.R
+++ R/subjects.R
@@ -7,12 +7,14 @@
 dst_subjects <- function(lang = "en", columns = c("id", "description")) {
   url <- url_api("subjects")
   lang <- lang_api(lang)
+  query <- list(lang = lang, format = "JSON")
+
   columns_values <- c("id", "description", "active", "hasSubjects", "subjects")
 
   if(!all(columns %in% columns_values)) {
     abort('Columns can only take the values: "id", "description", "active", "hasSubject" or
           "subjects"')
   }
-  GET_res <- GET(url, query = list(lang = lang, format = "JSON"))
+  GET_res <- GET(url, query)
   as_tibble(fromJSON(content(GET_res, "text"))[columns])
 }
