7650ab3617a66142bb9c4aaae94b34addd90c532
Jakob Jul Elben <elbenjakobjul@gmail.com>
2017-06-07 17:09:32 +0200

    Update documentation

--- R/subjects.R
+++ R/subjects.R
@@ -2,8 +2,21 @@
 #' @importFrom jsonlite fromJSON
 #' @importFrom rlang abort
 #' @importFrom tibble as_tibble
+NULL
 
+#' Retrieve subjects from Statistics Denmark
+#'
+#' You can use `dst_subject()` to get the subjects, which Statistics Denmark use to categorize
+#' their data sets.
+#'
+#' @param lang description language - "da" for danish and "en" for english.
+#' @param columns columns to return. Can take the values "id", "description", "active",
+#' "hasSubject" or "subjects".
+#'
+#' @return Tibble containing the specified columns in the chosen language.
 #' @export
+#' @examples
+#' View(dst_subjects())
 dst_subjects <- function(lang = "en", columns = c("id", "description")) {
   url <- modify_url_api("subjects")
   lang <- lang_api(lang)
